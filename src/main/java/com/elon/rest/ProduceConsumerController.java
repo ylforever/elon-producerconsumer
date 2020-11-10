b6930a7996c1caf558439eefb7270dec8c16be87
Elon <22001029@qq.com>
2020-11-11 00:40:06 +0800

    增加gzip压缩配置

--- src/main/java/com/elon/rest/ProduceConsumerController.java
+++ src/main/java/com/elon/rest/ProduceConsumerController.java
@@ -4,6 +4,7 @@ import com.elon.datamanager.TaskManager;
 import com.elon.service.ProConsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,16 @@ public class ProduceConsumerController {
     @ApiOperation(value = "终止任务")
     public void terminateTask() {
         TaskManager.instance().terminateTask();
+    }
+
+    @GetMapping("/get-string/{num}")
+    @ApiOperation(value = "获取压缩字符串")
+    public String getString(@PathVariable("num") int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; ++i) {
+            sb.append("A");
+        }
+
+        return sb.toString();
     }
 }
